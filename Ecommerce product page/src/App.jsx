c51051514e5c5c40ee916c097e7f712868d0f74b
Emmakin <101441304+Emmakin@users.noreply.github.com>
2023-05-03 20:37:36 +0100

    Working on the body of the product

--- Ecommerce product page/src/App.jsx
+++ Ecommerce product page/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Body from './components/body/body'
 import Header from './components/header/header'
 
 function App() {
   return (
     <>
       <Header />
+      <Body />
     </>
   )
 }
---
+++ Ecommerce product page/src/components/body/body.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './body.css'
+import Picture1 from '../image/image-product-1.jpg'
+import Thumbnail1 from '../image/image-product-1-thumbnail.jpg'
+import Thumbnail2 from '../image/image-product-2-thumbnail.jpg'
+import Thumbnail3 from '../image/image-product-3-thumbnail.jpg'
+import Thumbnail4 from '../image/image-product-4-thumbnail.jpg'
+
+function Body() {
+  return (
+    <div className='wrapperB'>
+        <div wrap-P>
+            <img src={Picture1} alt="This a picture of a shoe"/>
+            <ul>
+                <li><img src={Thumbnail1} alt= "The thumbnail for the frontview of the pic" /></li>
+                <li><img src={Thumbnail2} alt= "The thumbnail for the rightview of the pic" /></li>
+                <li><img src={Thumbnail3} alt= "The thumbnail for the leftview of the pic" /></li>
+                <li><img src={Thumbnail4} alt= "The thumbnail for the backview of the pic" /></li>
+            </ul>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Body
